5724b0050a9b5e150b527691275f0cad9b51a3ef
gtr3base <aloxaryabov@gmail.com>
2026-04-09 14:20:00 +0300

    init commit

---
+++ src/test/java/com/gtr3base/BCardManagingSys/BCardManagingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtr3base.BCardManagingSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BCardManagingSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
